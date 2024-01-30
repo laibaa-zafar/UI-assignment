94af31f5852c2f273b13fd16025f2ccf8e5d18b9
Laiba <laibazafar2699@gmail.com>
2024-01-30 18:57:55 +0500

    main

--- src/Components/AppWorking/AppWorking.jsx
+++ src/Components/AppWorking/AppWorking.jsx
@@ -9,23 +9,23 @@ const AppWorking = () => {
       </div>
       <div className="right-div">
         <h1> How does this App Work? </h1>
-        <div class="button-container">
-          <button class="button1">Passenger</button>
-          <button class="button2">Driver</button>
+        <div className="button-container">
+          <button className="button1">Passenger</button>
+          <button className="button2">Driver</button>
         </div>
-        <div class="image-container">
+        <div className="image-container">
           <img src="./Images/grouppicture.png" alt="" />
 
           <div className="main-text-container">
-            <div class="text-container">
+            <div className="text-container">
               <h3> Receive an offer</h3>
               <p> Specify your details and you will receive an offer </p>
             </div>
-            <div class="text-container">
+            <div className="text-container">
               <h3>Accept offer</h3>
               <p> The app will accept your offer</p>
             </div>
-            <div class="text-container">
+            <div className="text-container">
               <h3>Start your trip</h3>
               <p>Then you will start your trip</p>
             </div>
@@ -33,20 +33,26 @@ const AppWorking = () => {
         </div>
         <div className="button-apple">
           <button className="download-button">
-            Download on apple play store{" "}
+            <img
+              src="./Images/applestore.png"
+              alt="Apple"
+              className="button-icon"
+            />
+            Download on <br />
+            Apple Play Store
           </button>
-        
-        {/* <div className="download-playstore"> */}
           <button className="download-button">
-            Download on the google PlayStore{" "}
+            <img
+              src="./Images/playstore.png"
+              alt="PlayStore"
+              className="button-icon"
+            />
+            Download on <br />
+            Play Store
           </button>
         </div>
-
       </div>
-      </div>
-
-      // </div>
-    
+    </div>
   );
 };
 
